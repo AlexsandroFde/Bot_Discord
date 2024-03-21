5e0e1879365af2c235b7b5a85648f0a476048d49
AlexsandroFde <alexsandro.ferreira02@aluno.ifce.edu.br>
2024-03-21 12:58:55 -0300

    import democracy command

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from bot.client import aclient
 from bot.commands.moderation.mensagem import message
 from bot.commands.moderation.clear import clear_messages
 from bot.commands.moderation.ditadura import ditadura
+from bot.commands.moderation.democracy import democracy
 from bot.commands.mute.mute import mute
 from bot.commands.mute.unmute import unmute
 from bot.commands.spam.spam import spam_message
